a383663bb4de61bf6831a42613a009dff30c3e10
Maciej Ziółkowski <ziolek0321@gmail.com>
2014-05-29 19:13:36 +0200

    Dodanie do procedury GO, zmiana budowy procedury z uzyciem Common Table Expressions

--- procedury/P01.sql
+++ procedury/P01.sql
@@ -7,7 +7,37 @@
   
 CREATE PROCEDURE staly_3
 AS BEGIN
-	UPDATE Klient SET staly_klient=1 FROM Zamowienia z JOIN Klient k ON (z.id_klienta=k.id_klient ) GROUP BY staly_klient  HAVING COUNT(z_id_zamowienia)=4 ;
+
+	WITH ilosc_transakcji_CTE 
+	(
+		cte_id_klient
+	)
+	AS
+	(
+		SELECT
+			id_klient
+		FROM
+			Zamowienia z
+			JOIN
+			Klient k
+			ON (z.id_klienta=k.id_klient) 
+		GROUP BY
+			k.id_klient
+		HAVING
+			COUNT(z.id_zamowienia) > 4	
+	)
+	UPDATE
+		k
+	SET
+		staly_klient =1 
+	FROM
+	Klient k 
+	JOIN
+	ilosc_transakcji_CTE
+	ON (k.id_klient=cte_id_klient)
 END
-EXEC staly_2;
+
+-- przyklad
+EXEC staly_3;
+GO
 
