cfe2895afb7285b38adad4e1d5b3103605c695e9
Maciej Ziółkowski <ziolek0321@gmail.com>
2014-05-31 22:00:28 +0200

    Uzupełniłem tabele transport i zamowienia

---
+++ init/init_transport.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO Transport VALUES 
+(1001,'UPC','Kurier'),
+(1002,'DHL','Kurier'),
+(1003,'FedEX','Kurier'),
+(1004,'Poczta Polska','Paczka 48'),
+(1005,'Poczta Polska','Paczka 24'),
+(1006,'Poczta Polska','Paczka pocztowa priorytetowa');
+
+GO
